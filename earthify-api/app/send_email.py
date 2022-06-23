0a40f3787e76e55d9361c21989eaa378d9cc77ca
wahab <wahabch.ch@gmail.com>
2022-06-23 05:53:14 +0500

    email configs added

--- earthify-api/app/send_email.py
+++ earthify-api/app/send_email.py
@@ -1,18 +1,15 @@
-import os
 from fastapi import BackgroundTasks
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
-from dotenv import load_dotenv
-
-load_dotenv('./.env')
+from decouple import config
 
 
 class Envs:
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_FROM = os.getenv('MAIL_FROM')
-    MAIL_PORT = int(os.getenv('MAIL_PORT'))
-    MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_FROM_NAME = os.getenv('MAIN_FROM_NAME')
+    MAIL_USERNAME = config('MAIL_USERNAME')
+    MAIL_PASSWORD = config('MAIL_PASSWORD')
+    MAIL_FROM = config('MAIL_FROM')
+    MAIL_PORT = int(config('MAIL_PORT'))
+    MAIL_SERVER = config('MAIL_SERVER')
+    MAIL_FROM_NAME = config('MAIL_FROM_NAME')
 
 
 conf = ConnectionConfig(
