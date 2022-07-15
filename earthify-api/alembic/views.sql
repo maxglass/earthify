9f0ba28b48d07744a3e20743aa856ff1c9932d79
wahab <wahabch.ch@gmail.com>
2022-07-15 22:49:02 +0500

    external queries

--- earthify-api/alembic/views.sql
+++ earthify-api/alembic/views.sql
@@ -20,9 +20,6 @@ CREATE OR REPLACE VIEW public.county_names
      LEFT JOIN foo ON counties."NAME"::text = foo.county::text
   ORDER BY (COALESCE(foo.count, 0::bigint));
 
-ALTER TABLE public.county_names
-    OWNER TO postgres;
-
 
 
 -- View: public.county_count
@@ -48,4 +45,90 @@ CREATE OR REPLACE VIEW public.county_count
 
 ALTER TABLE public.county_count
     OWNER TO postgres;
+
+
+
+CREATE OR REPLACE FUNCTION public.tbl_counties(
+	z integer,
+	x integer,
+	y integer)
+    RETURNS bytea
+    LANGUAGE 'sql'
+    COST 100
+    STABLE PARALLEL SAFE
+AS $BODY$
+	WITH
+bounds AS (
+    SELECT ST_TileEnvelope(z, x, y) AS geom,
+           ST_TileEnvelope(z, x, y)::box2d AS b2d
+),
+mvtgeom AS (
+    SELECT ST_AsMVTGeom(ST_Transform(t.geometry, 3857), bounds.b2d) AS geom,
+           "NAME", t.count
+    FROM public.county_names t, bounds
+    WHERE ST_Intersects(ST_Transform(t.geometry, 3857), ST_Transform(bounds.geom, 3857))
+
+)
+SELECT ST_AsMVT(mvtgeom.*) FROM mvtgeom;
+
+$BODY$;
+
+
+
+CREATE OR REPLACE FUNCTION public.tbl_grid(
+	z integer,
+	x integer,
+	y integer)
+    RETURNS bytea
+    LANGUAGE 'sql'
+    COST 100
+    STABLE PARALLEL SAFE
+AS $BODY$
+	WITH
+bounds AS (
+    SELECT ST_TileEnvelope(z, x, y) AS geom,
+           ST_TileEnvelope(z, x, y)::box2d AS b2d
+),
+mvtgeom AS (
+    SELECT ST_AsMVTGeom(ST_Transform(t.geometry, 3857), bounds.b2d) AS geom,
+           col1,col2,col3
+    FROM public.data t, bounds
+    WHERE ST_Intersects(ST_Transform(t.geometry,3857),ST_Transform(bounds.geom,3857))
+)
+SELECT ST_AsMVT(mvtgeom.*) FROM mvtgeom;
+
+$BODY$;
+
+
+-- Table: public.states
+
+-- DROP TABLE IF EXISTS public.states;
+
+CREATE TABLE IF NOT EXISTS public.states
+(
+    id integer NOT NULL,
+    "NAME" character varying COLLATE pg_catalog."default",
+    geometry geometry(Geometry,4326) NOT NULL
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.states
+    OWNER to postgres;
+-- Index: idx_states_geometry
+
+-- DROP INDEX IF EXISTS public.idx_states_geometry;
+
+CREATE INDEX IF NOT EXISTS idx_states_geometry
+    ON public.states USING gist
+    (geometry)
+    TABLESPACE pg_default;
+-- Index: ix_states_id
+
+-- DROP INDEX IF EXISTS public.ix_states_id;
+
+CREATE INDEX IF NOT EXISTS ix_states_id
+    ON public.states USING btree
+    (id ASC NULLS LAST)
+    TABLESPACE pg_default;
 
